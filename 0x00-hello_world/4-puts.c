7ce1a3ca20260e55c1230437af48a42f9804af4b
ajasaolasunkanmi <ajasa_o@yahoo.com>
2022-10-13 21:55:27 -0700

    cccc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry
- * return: Always 0
+ * main - Entry
+ * Return: Always 0
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
+}
